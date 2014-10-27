9057549d6890afd0a6426629634704e30a879a4d
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2014-10-27 22:49:47 +1000

    Face and color detection working and toggleable.

--- src/scripts/services/robot.service.js
+++ src/scripts/services/robot.service.js
@@ -24,105 +24,10 @@ exports.Service = service.extend({
         var self = this;
 
         self.state = self.STATES.AUTO;
-
-        // self.canvas = document.getElementById("canvas");
-        // self.ctx = self.canvas.getContext("2d");
+        self.trackColors = false;
+        self.trackFaces = false;
 
         self.robot = new Robot(self.scope.$routeParams.robotId);
-
-
-
-
-
-
-        var wrapper = document.getElementById('robot-control');
-
-        function createStream(webcamId) {
-            var video = document.createElement('video');
-            var canvas = document.createElement('canvas');
-            canvas.setAttribute('width', '800');
-            canvas.setAttribute('height', '800');
-            var ctx = canvas.getContext('2d');
-
-            wrapper.appendChild(canvas);
-
-            function successCallback(stream) {
-                var domURL = window.URL || window.webkitURL;
-                video.src = domURL ? domURL.createObjectURL(stream) : stream;
-
-                setInterval(function () {
-                    video.play();
-
-                    if (canvas.width != video.videoWidth && video.videoWidth != 0) {
-                        canvas.setAttribute('width', video.videoWidth.toString());
-                        canvas.setAttribute('height', video.videoHeight.toString());
-                    }
-
-
-                    ctx.drawImage(video, 0, 0);
-
-                    var img = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-                    for (var i = 0; i < img.data.length; i += 4) {
-                        // img.data[i] = 255 - img.data[i];
-                        // img.data[i + 1] = 255 - img.data[i + 1];
-                        // img.data[i + 2] = 255 - img.data[i + 2];
-                    }
-
-                    var colors = new tracking.ColorTracker(['cyan']);
-                    var objects = new tracking.ObjectTracker(['face']);
-                    objects.setInitialScale(4);
-                    objects.setStepSize(2);
-                    objects.setEdgesDensity(0.1);
-
-                    colors.on('track', function (event) {
-                        if (event.data.length === 0) {
-                            // No colors were detected in this frame.
-                        } else {
-                            event.data.forEach(function (rect) {
-                                // console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
-                                ctx.strokeRect(rect.x, rect.y, rect.height, rect.width);
-                            });
-                        }
-                    });
-
-                    objects.on('track', function (event) {
-                        if (event.data.length === 0) {
-                            // No objects were detected in this frame.
-                        } else {
-                            event.data.forEach(function (rect) {
-                                // rect.x, rect.y, rect.height, rect.width
-                                ctx.strokeRect(rect.x, rect.y, rect.height, rect.width);
-                            });
-                        }
-                    });
-
-                    // tracking.track(canvas, colors);
-                    tracking.track(canvas, objects);
-
-
-                    // ctx.putImageData(img, 0, 0);
-
-                }, 16);
-
-
-            }
-
-            function errorCallback(error) {
-                console.error('An error occurred: [CODE ' + error.code + ']');
-                return;
-            }
-
-            navigator.getUserMedia({
-                // video: {
-                //     mandatory: {
-                //         sourceId: webcamId
-                //     }
-                // },
-                video: true,
-                audio: false
-            }, successCallback, errorCallback);
-        }
 
 
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -134,7 +39,7 @@ exports.Service = service.extend({
                     var source = sources[i];
                     if (source.kind == 'video') {
                         console.log(sources[i]);
-                        createStream(sources[i].id);
+                        self.createStream(sources[i].id);
                         return;
                     }
                 }
@@ -145,5 +50,116 @@ exports.Service = service.extend({
             return;
         }
 
+    },
+
+    createStream: function (webcamId) {
+        var self = this;
+
+        navigator.getUserMedia({
+            // video: { //Example of specifying id for using multiple cameras.
+            //     mandatory: {
+            //         sourceId: webcamId
+            //     }
+            // },
+            video: true,
+            audio: false
+        }, function (stream) {
+            self.cameraSuccess(stream);
+        }, function (error) {
+            console.error('An error occurred: [CODE ' + error.code + ']');
+        });
+    },
+
+    cameraSuccess: function (stream) {
+        var self = this;
+        var wrapper = document.getElementById('robot-control');
+        var video = document.createElement('video');
+        var canvas = document.createElement('canvas');
+        canvas.setAttribute('width', '800');
+        canvas.setAttribute('height', '800');
+        var ctx = canvas.getContext('2d');
+
+        wrapper.appendChild(canvas);
+        var domURL = window.URL || window.webkitURL;
+        video.src = domURL ? domURL.createObjectURL(stream) : stream;
+
+        var faces = [];
+        var colors = [];
+        var frameCounter = 0;
+        setInterval(function () {
+            video.play();
+            frameCounter++;
+
+            if (canvas.width != video.videoWidth && video.videoWidth != 0) {
+                canvas.setAttribute('width', video.videoWidth.toString());
+                canvas.setAttribute('height', video.videoHeight.toString());
+            }
+
+            ctx.drawImage(video, 0, 0);
+
+            //Only look for items once per second
+            if (frameCounter % 60 == 0) {
+                self.findFaces(canvas, colors, faces);
+            }
+
+            if (self.trackFaces) {
+                self.drawRects(ctx, faces);
+            }
+            if (self.trackColors) {
+                self.drawRects(ctx, colors);
+            }
+
+        }, 16);
+    },
+
+    drawRects: function (ctx, rects) {
+        for (var i = 0; i < rects.length; i++) {
+            var rect = rects[i];
+            if (rect.color) {
+                ctx.strokeStyle = rect.color;
+            } else {
+                ctx.strokeStyle = "#000000";
+            }
+            ctx.strokeRect(rect.x, rect.y, rect.height, rect.width);
+        }
+    },
+
+    findFaces: function (canvas, colors, faces) {
+        var self = this;
+        var detectColors = new tracking.ColorTracker(['cyan']); //'magenta', 'cyan', 'yellow'
+        var detectFaces = new tracking.ObjectTracker(['face']);
+        detectFaces.setInitialScale(4);
+        detectFaces.setStepSize(2);
+        detectFaces.setEdgesDensity(0.1);
+
+        detectColors.on('track', function (event) {
+            colors.splice(0);
+            if (event.data.length === 0) {
+                // No detectColors were detected in this frame.
+            } else {
+                event.data.forEach(function (rect) {
+                    colors.push(rect);
+                });
+            }
+        });
+
+        detectFaces.on('track', function (event) {
+            faces.splice(0);
+            if (event.data.length === 0) {
+                // No detectFaces were detected in this frame.
+            } else {
+                event.data.forEach(function (rect) {
+                    // rect.x, rect.y, rect.height, rect.width
+                    faces.push(rect);
+                });
+            }
+        });
+
+        if (self.trackColors) {
+            tracking.track(canvas, detectColors);
+        }
+        if (self.trackFaces) {
+            tracking.track(canvas, detectFaces);
+        }
     }
 });
